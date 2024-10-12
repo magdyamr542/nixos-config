ece06228feeaec5c1f9bade191b25ddb2826093c
amr <magdyamr542@gmail.com>
2024-10-12 22:17:24 +0200

    update notification dunst configuration

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -83,7 +83,26 @@
   # Notification system
   services.dunst = {
     enable = true;
+
     settings = {
+      global = {
+        follow = "mouse";
+        font = "Droid Sans 10";
+        format = "<b>%s</b>\\n%b";
+        frame_color = "#555555";
+        frame_width = 2;
+        geometry = "500x5-5+30";
+        horizontal_padding = 8;
+        icon_position = "off";
+        line_height = 0;
+        markup = "full";
+        padding = 8;
+        separator_color = "frame";
+        separator_height = 2;
+        transparency = 10;
+        word_wrap = true;
+      };
+
       urgency_low = {
         background = "#1d1f21";
         foreground = "#4da1af";
