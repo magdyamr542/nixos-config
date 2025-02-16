4fa3fb14de71ade34b263482e04499623eba9c78
amr <magdyamr542@gmail.com>
2025-02-16 22:27:00 +0100

    update to new neovim commit which uses the rust lsp

--- home-manager/neovim.nix
+++ home-manager/neovim.nix
@@ -10,8 +10,8 @@ let
   nvimConfigDir = pkgs.fetchFromGitHub {
     owner = "magdyamr542";
     repo = "nvim";
-    rev = "27a364bf487a0de7afc7f5971721879e328c4ef4";
-    hash = "sha256-mLnPHENTFTnGDBhpPoDzDZoNW1dzWR5JUfke+q4iiWU=";
+    rev = "0992f51cb23d6b270e530755057ae81b966fc3cf";
+    hash = "sha256-I34lxqBoJpfUJwOpWFO16u6VvS/90X4p5a64BRk0yPI=";
   };
 in
 {
