696e2787ad70d024dc5ab744f9221232aa2cb18a
Amr Metwally <amr.metwally@lynq.tech>
2026-09-10 22:35:26 +0200

    Migrate cloud, security, database, and language-toolchain CLIs to Nix

--- home/packages.nix
+++ home/packages.nix
@@ -3,8 +3,11 @@
 {
   # Add or remove user-level CLI packages here, then run `make apply`.
   home.packages = with pkgs; [
+    awscli2
     bat
+    bitwarden-cli
     curl
+    docker-client
     eza
     fd
     gh
@@ -12,11 +15,22 @@
     htop
     httpie
     jq
+    kubectx
+    libpq
+    mirrord
+    nmap
+    pgbouncer
     protobuf
+    python311
     rename
     ripgrep
+    tenv
+    tesseract
+    tflint
+    tfsec
     tldr
     tree
+    trivy
     wget
     yq-go
   ];
